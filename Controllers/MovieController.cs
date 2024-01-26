2811c2a258e025f81c94f880566bca59b28989e4
amedipiran <robertopiranamedi@gmail.com>
2024-01-26 12:58:53 +0100

    lagt till metod för att rensa lista och enstaka filmer

--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -26,11 +26,10 @@ namespace Moment_2.Controllers
 
         [HttpPost]
 
+//Metod för att lägga till film
 public IActionResult SaveMovie(Movie movie)
 
 {
-
-
     if (string.IsNullOrEmpty(movie.Title) || string.IsNullOrEmpty(movie.Director))
     {
         // Om något av fälten är tomt
@@ -44,7 +43,7 @@ public IActionResult SaveMovie(Movie movie)
             movieList = JsonConvert.DeserializeObject<List<Movie>>(movieListJson);
         }
 
-        // Återvänd till vyn med samma modell (befintlig lista) för att visa felmeddelandet
+        // Återvänd till vyn 
         return View("Index", movieList);
     }
 
@@ -73,7 +72,49 @@ public IActionResult SaveMovie(Movie movie)
     return View("Index", new List<Movie>());
 }
 
+[HttpPost]
+public IActionResult ClearList()
+{
+    // Rensa hela listan genom att ersätta den med en tom lista
+    var movieList = new List<Movie>();
+    
+    // Spara den tomma listan till cookies
+    CookieOptions options = new CookieOptions
+    {
+        Expires = DateTime.Now.AddDays(1)
+    };
+    Response.Cookies.Append("MovieList", JsonConvert.SerializeObject(movieList), options);
 
+    return RedirectToAction("Index");
+}
 
+//radera enstaka filmer från listan
+public IActionResult DeleteMovie(int id)
+{
+    var movieList = new List<Movie>();
+    var movieListJson = Request.Cookies["MovieList"];
+
+    if (!string.IsNullOrEmpty(movieListJson))
+    {
+        movieList = JsonConvert.DeserializeObject<List<Movie>>(movieListJson);
     }
+
+    // Hitta filmen med det angivna ID och ta bort den från listan
+    var movieToRemove = movieList.FirstOrDefault(m => m.Id == id);
+    if (movieToRemove != null)
+    {
+        movieList.Remove(movieToRemove);
+        
+        // Spara den uppdaterade listan till cookies
+        CookieOptions options = new CookieOptions
+        {
+            Expires = DateTime.Now.AddDays(1)
+        };
+        Response.Cookies.Append("MovieList", JsonConvert.SerializeObject(movieList), options);
+    }
+
+    return RedirectToAction("Index");
+}
+    }
+    
 }
--- Models/Movie.cs
+++ Models/Movie.cs
@@ -4,6 +4,8 @@ namespace Moment_2.Models
 {
     public class Movie
     {
+          public int Id { get; set; } 
+
         //Validering för fälten
         [Required(ErrorMessage = "Titeln får inte vara tom")]
         public string Title { get; set; }
