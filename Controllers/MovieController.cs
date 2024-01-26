01e107db9c4aef02ad11b48fdf2dc0b6b18ad986
amedipiran <robertopiranamedi@gmail.com>
2024-01-26 13:06:51 +0100

    build inställningar

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,120 @@
+using Microsoft.AspNetCore.Mvc;
+using Moment_2.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace Moment_2.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Index()
+        {
+                ViewBag.Message = "Välkommen till filmsidan!";
+                ViewData["Greeting"] = "Här kan du spara dina favoritfilmer.";
+            var movieList = new List<Movie>();
+            //Hämtar cookies
+            var movieListJson = Request.Cookies["MovieList"];
+
+            if (!string.IsNullOrEmpty(movieListJson))
+            {
+                movieList = JsonConvert.DeserializeObject<List<Movie>>(movieListJson);
+            }
+
+            return View(movieList);
+        }
+
+        [HttpPost]
+
+//Metod för att lägga till film
+public IActionResult SaveMovie(Movie movie)
+
+{
+    if (string.IsNullOrEmpty(movie.Title) || string.IsNullOrEmpty(movie.Director))
+    {
+        // Om något av fälten är tomt
+        ModelState.AddModelError(string.Empty, "Fyll i alla fält.");
+        
+        var movieList = new List<Movie>();
+        var movieListJson = Request.Cookies["MovieList"];
+
+        if (!string.IsNullOrEmpty(movieListJson))
+        {
+            movieList = JsonConvert.DeserializeObject<List<Movie>>(movieListJson);
+        }
+
+        // Återvänd till vyn 
+        return View("Index", movieList);
+    }
+
+    if (ModelState.IsValid)
+    {
+        var movieList = new List<Movie>();
+        var movieListJson = Request.Cookies["MovieList"];
+
+        if (!string.IsNullOrEmpty(movieListJson))
+        {
+            movieList = JsonConvert.DeserializeObject<List<Movie>>(movieListJson);
+        }
+
+        movieList.Add(movie);
+
+        CookieOptions options = new CookieOptions
+        {
+            Expires = DateTime.Now.AddDays(1)
+        };
+        Response.Cookies.Append("MovieList", JsonConvert.SerializeObject(movieList), options);
+
+        return RedirectToAction("Index");
+    }
+
+    // Om valideringen misslyckas, återvänd till vyn med samma modell
+    return View("Index", new List<Movie>());
+}
+
+[HttpPost]
+public IActionResult ClearList()
+{
+    // Rensa hela listan genom att ersätta den med en tom lista
+    var movieList = new List<Movie>();
+    
+    // Spara den tomma listan till cookies
+    CookieOptions options = new CookieOptions
+    {
+        Expires = DateTime.Now.AddDays(1)
+    };
+    Response.Cookies.Append("MovieList", JsonConvert.SerializeObject(movieList), options);
+
+    return RedirectToAction("Index");
+}
+
+//radera enstaka filmer från listan
+public IActionResult DeleteMovie(int id)
+{
+    var movieList = new List<Movie>();
+    var movieListJson = Request.Cookies["MovieList"];
+
+    if (!string.IsNullOrEmpty(movieListJson))
+    {
+        movieList = JsonConvert.DeserializeObject<List<Movie>>(movieListJson);
+    }
+
+    // Hitta filmen med det angivna ID och ta bort den från listan
+    var movieToRemove = movieList.FirstOrDefault(m => m.Id == id);
+    if (movieToRemove != null)
+    {
+        movieList.Remove(movieToRemove);
+        
+        // Spara den uppdaterade listan till cookies
+        CookieOptions options = new CookieOptions
+        {
+            Expires = DateTime.Now.AddDays(1)
+        };
+        Response.Cookies.Append("MovieList", JsonConvert.SerializeObject(movieList), options);
+    }
+
+    return RedirectToAction("Index");
+}
+    }
+    
+}
